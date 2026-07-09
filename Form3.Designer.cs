8a02ddadd98928edd0aa84e7ee77b0e96c2c0a89
Manzikevin <manzikevin04@gmail.com>
2026-07-09 17:18:27 +0200

    Adding Course Working Perfectly

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,32 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvCourses = new System.Windows.Forms.DataGridView();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CourseCode = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CourseName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Credits = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvCourses
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(478, 444);
-            this.dataGridView1.TabIndex = 17;
+            this.dgvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCourses.Location = new System.Drawing.Point(12, 26);
+            this.dgvCourses.Name = "dgvCourses";
+            this.dgvCourses.Size = new System.Drawing.Size(478, 444);
+            this.dgvCourses.TabIndex = 17;
+            this.dgvCourses.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button4
             // 
@@ -65,6 +66,7 @@
             this.button4.TabIndex = 21;
             this.button4.Text = "Clear";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -77,6 +79,7 @@
             this.button3.TabIndex = 20;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -89,6 +92,7 @@
             this.button2.TabIndex = 19;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -101,6 +105,7 @@
             this.button1.TabIndex = 18;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -112,13 +117,13 @@
             this.label2.Text = "Course Code";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox2
+            // CourseCode
             // 
-            this.textBox2.Location = new System.Drawing.Point(654, 86);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(207, 33);
-            this.textBox2.TabIndex = 22;
+            this.CourseCode.Location = new System.Drawing.Point(654, 86);
+            this.CourseCode.Multiline = true;
+            this.CourseCode.Name = "CourseCode";
+            this.CourseCode.Size = new System.Drawing.Size(207, 33);
+            this.CourseCode.TabIndex = 22;
             // 
             // label1
             // 
@@ -130,13 +135,13 @@
             this.label1.Text = "Course Title";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox1
+            // CourseName
             // 
-            this.textBox1.Location = new System.Drawing.Point(654, 149);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(207, 33);
-            this.textBox1.TabIndex = 24;
+            this.CourseName.Location = new System.Drawing.Point(654, 149);
+            this.CourseName.Multiline = true;
+            this.CourseName.Name = "CourseName";
+            this.CourseName.Size = new System.Drawing.Size(207, 33);
+            this.CourseName.TabIndex = 24;
             // 
             // label3
             // 
@@ -148,13 +153,13 @@
             this.label3.Text = "Course Credit";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textBox3
+            // Credits
             // 
-            this.textBox3.Location = new System.Drawing.Point(654, 217);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(207, 33);
-            this.textBox3.TabIndex = 26;
+            this.Credits.Location = new System.Drawing.Point(654, 217);
+            this.Credits.Multiline = true;
+            this.Credits.Name = "Credits";
+            this.Credits.Size = new System.Drawing.Size(207, 33);
+            this.Credits.TabIndex = 26;
             // 
             // button5
             // 
@@ -218,19 +223,20 @@
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Credits);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CourseName);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.CourseCode);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvCourses);
             this.Name = "Form3";
             this.Text = "Course Registration";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Form3_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,17 +244,17 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvCourses;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox CourseCode;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox CourseName;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox Credits;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox4;
--- Form3.cs
+++ Form3.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,258 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Mysqlx.Datatypes.Scalar.Types;
 
 namespace UniRegstrationSys
 {
     public partial class Form3 : Form
     {
+        // Initializes the form and its designer-generated visual components.
         public Form3()
         {
             InitializeComponent();
+        }
+
+        string connString = "Server=localhost;Database=universityreg;Uid=root;Pwd=;";
+
+        // Clears all entry fields on the form and refocuses the cursor onto the course code field.
+        private void ClearFormFields()
+        {
+            CourseCode.Clear();
+            CourseName.Clear();
+            Credits.Clear();
+            CourseCode.Focus();
+        }
+
+        // Queries the database to retrieve all registered course columns into a structured DataTable object.
+        public DataTable GetCourseList()
+        {
+            string query = "SELECT CourseID, CourseCode, CourseName, Credits FROM Courses";
+            DataTable dataTable = new DataTable();
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    try
+                    {
+                        conn.Open();
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(dataTable);
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Error loading data: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        // Binds the updated database list to the DataGridView control and configures clean user column names.
+        private void RefreshCourseGrid()
+        {
+            DataTable dt = GetCourseList();
+            dgvCourses.DataSource = dt;
+
+            dgvCourses.Columns["CourseID"].HeaderText = "ID";
+            dgvCourses.Columns["CourseCode"].HeaderText = "Course Code";
+            dgvCourses.Columns["CourseName"].HeaderText = "Course Name";
+            dgvCourses.Columns["Credits"].HeaderText = "Credits";
+        }
+
+        // Triggers initial data loading to populate the grid as soon as the Form3 runtime window opens.
+
+        private void Form3_Load_1(object sender, EventArgs e)
+        {
+            RefreshCourseGrid();
+        }
+
+        // Executes an INSERT command using safe parameters to save a new course record to MySQL.
+        public bool SaveCourse(string courseCode, string courseName, int credits)
+        {
+            string query = @"INSERT INTO Courses (CourseCode, CourseName, Credits) 
+                             VALUES (@CourseCode, @CourseName, @Credits);";
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseCode", courseCode);
+                    cmd.Parameters.AddWithValue("@CourseName", courseName);
+                    cmd.Parameters.AddWithValue("@Credits", credits);
+
+                    try
+                    {
+                        conn.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        if (ex.Number == 1062)
+                        {
+                            MessageBox.Show("This Course Code already exists.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Database Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // Captures selected grid row field values and maps data back to the entry form textboxes when a cell is clicked.
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvCourses.Rows[e.RowIndex];
+
+                CourseCode.Text = row.Cells["CourseCode"].Value?.ToString();
+                CourseName.Text = row.Cells["CourseName"].Value?.ToString();
+                Credits.Text = row.Cells["Credits"].Value?.ToString();
+            }
+        }
+
+        // Validates form input data, extracts text, and attempts to save a new course on button click.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(CourseCode.Text) || string.IsNullOrWhiteSpace(CourseName.Text))
+            {
+                MessageBox.Show("Please fill in all required fields (Course Code and Course Name).",
+                                "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(Credits.Text, out int courseCredits))
+            {
+                MessageBox.Show("Please enter a valid numeric integer value for Credits.",
+                                "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string code = CourseCode.Text.Trim();
+            string name = CourseName.Text.Trim();
+
+            if (SaveCourse(code, name, courseCredits))
+            {
+                MessageBox.Show("Course registered successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearFormFields();
+                RefreshCourseGrid();
+            }
+        }
+
+        // Executes an UPDATE database transaction to modify existing course attributes matching the given code.
+        public bool UpdateCourse(string courseCode, string courseName, int credits)
+        {
+            string query = @"UPDATE Courses 
+                             SET CourseName = @CourseName, Credits = @Credits 
+                             WHERE CourseCode = @CourseCode;";
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseCode", courseCode);
+                    cmd.Parameters.AddWithValue("@CourseName", courseName);
+                    cmd.Parameters.AddWithValue("@Credits", credits);
+
+                    try
+                    {
+                        conn.Open();
+                        return cmd.ExecuteNonQuery() > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // Validates updated textbox strings, parses numbers, and edits the target course record on button click.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(CourseCode.Text))
+            {
+                MessageBox.Show("Please select a course from the grid to update.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(Credits.Text, out int courseCredits))
+            {
+                MessageBox.Show("Please enter a valid numeric integer value for Credits.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string code = CourseCode.Text.Trim();
+            string name = CourseName.Text.Trim();
+
+            if (UpdateCourse(code, name, courseCredits))
+            {
+                MessageBox.Show("Course updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearFormFields();
+                RefreshCourseGrid();
+            }
+        }
+
+        // Executes a DELETE database transaction targeting a single course record based on its unique code string.
+        public bool DeleteCourse(string courseCode)
+        {
+            string query = "DELETE FROM Courses WHERE CourseCode = @CourseCode;";
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseCode", courseCode);
+
+                    try
+                    {
+                        conn.Open();
+                        return cmd.ExecuteNonQuery() > 0;
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Database Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+        }
+        // Verifies context via user confirmation prompt prior to executing the course elimination transaction.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string code = CourseCode.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                MessageBox.Show("Please select a course from the grid to delete.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult dialogResult = MessageBox.Show($"Are you sure you want to delete course {code}?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (DeleteCourse(code))
+                {
+                    MessageBox.Show("Course deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ClearFormFields();
+                    RefreshCourseGrid();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ClearFormFields() ;
         }
     }
 }
